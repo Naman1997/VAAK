5ef6b0b9e261b2ee4cafdaa863e3db3d31911cde
Naman Arora <aroranaman17@outlook.com>
2019-03-26 02:13:45 +0530

    Update rev.py

--- rev.py
+++ rev.py
@@ -7,19 +7,18 @@ import time
 
 #print("Debug1")
 
-cred = credentials.Certificate('/home/pi/Documents/firebase/vaak-eeda0-firebase-adminsdk-13upq-49eac33bac.json')
+cred = credentials.Certificate('#Location of your private key generated in firebase(a json file)')
 #default_app = firebase_admin.initialize_app(cred)
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://vaak-eeda0.firebaseio.com/'
+    'databaseURL': '#Link_to_your_realtime_database'
 })
 
 
 while True:
-    ser = serial.Serial('/dev/ttyACM0',9600)
+    ser = serial.Serial('/dev/ttyACM0',9600)#9600 is the baud rate
     ref = db.reference()
     read_serial=ser.readline()
     print read_serial
     ref.update({
         'Data':read_serial
     })
-    #ser.close()
